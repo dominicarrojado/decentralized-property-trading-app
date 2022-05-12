51bd42119d12527994305542422d384b9c41845b
Sudarsan Padmanabhan <sudarsan@propertyguru.com.sg>
2022-05-12 20:51:31 +0530

    token generation on create property

--- pages/properties/create.page.tsx
+++ pages/properties/create.page.tsx
@@ -1,5 +1,93 @@
-import React from 'react';
+import React, { useContext, useMemo, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import { getContract } from '../../lib/eth';
+import MetaMaskConnected from '../../components/metaMaskConnected';
+import StoreContext from '../../lib/context';
+import { CONTRACT_CREATE_PROP_GAS } from '../../lib/constants';
+import { FetchState } from '../../lib/types';
+import { Alert, Badge } from 'react-bootstrap';
 
 export default function CreateProperty() {
-  return <div>This is the create property page</div>;
+  const [fetchState, setFetchState] = useState(FetchState.DEFAULT);
+  const [message, setMessage] = useState({
+    destinationAcc: null,
+    tokensCount: null,
+  });
+  const context = useContext(StoreContext);
+
+  const tokenizeProperty = async () => {
+    try {
+      setFetchState(FetchState.LOADING);
+
+      const contract = await getContract({
+        from: context.account?.addressId,
+        gas: CONTRACT_CREATE_PROP_GAS,
+      });
+      const res = await contract.methods
+        .createProperty(10000000, 23451242, '550321', 10)
+        .send();
+
+      setMessage({
+        destinationAcc: res.events.Transfer.returnValues.to,
+        tokensCount: res.events.Transfer.returnValues.value,
+      });
+      setFetchState(FetchState.SUCCESS);
+    } catch (err) {
+      console.error(err);
+      setFetchState(FetchState.ERROR);
+    }
+  };
+
+  const buttonText = useMemo(() => {
+    switch (fetchState) {
+      case FetchState.LOADING:
+        return 'Waiting...';
+
+      case FetchState.ERROR:
+        return 'Try Again';
+
+      default:
+        return 'Confirm Token Genration';
+    }
+  }, [fetchState]);
+
+  const buttonVariant = useMemo(() => {
+    switch (fetchState) {
+      case FetchState.LOADING:
+        return 'warning';
+
+      case FetchState.ERROR:
+        return 'danger';
+
+      default:
+        return 'primary';
+    }
+  }, [fetchState]);
+
+  return (
+    <MetaMaskConnected>
+      <div>
+        <Button variant={buttonVariant} onClick={tokenizeProperty}>
+          {buttonText}
+        </Button>
+      </div>
+      <br />
+      {message.destinationAcc && message.tokensCount && (
+        <div>
+          <Alert variant="success">
+            <Alert.Heading>Tokens Generated</Alert.Heading>
+            <div>
+              <span>Account: </span>
+              <span>{message.destinationAcc}</span>
+            </div>
+
+            <div>
+              <span>Tokens: </span>
+              <Badge bg="secondary">{message.tokensCount}</Badge>
+            </div>
+          </Alert>
+        </div>
+      )}
+    </MetaMaskConnected>
+  );
 }
