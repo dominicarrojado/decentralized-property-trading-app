34ca9fbfc5a0aaeb431b4995fd4d618099637e99
Dominic Arrojado <dominicarrojado@gmail.com>
2022-05-12 22:09:38 +0800

    create property contract call

--- components/contractCaller.tsx
+++ components/contractCaller.tsx
@@ -1,19 +1,25 @@
-import React, { useMemo, useState } from 'react';
+import React, { useContext, useMemo, useState } from 'react';
 import { Alert, Button } from 'react-bootstrap';
+import { CONTRACT_CREATE_PROP_GAS } from '../lib/constants';
+import StoreContext from '../lib/context';
 import { getContract } from '../lib/eth';
 import { FetchState } from '../lib/types';
 
 export default function ContractCaller() {
+  const context = useContext(StoreContext);
   const [fetchState, setFetchState] = useState(FetchState.DEFAULT);
   const [message, setMessage] = useState('');
   const getMessage = async () => {
     try {
       setFetchState(FetchState.LOADING);
 
-      const contract = await getContract();
-      const res = await contract.methods.getMessage().call();
+      const contract = await getContract({
+        from: context.account?.addressId,
+        gas: CONTRACT_CREATE_PROP_GAS,
+      });
+      const res = await contract.methods.createProperty(1, 1, '1', 1000).send();
 
-      setMessage(res);
+      setMessage(JSON.stringify(res));
       setFetchState(FetchState.SUCCESS);
     } catch (err) {
       console.error(err);
@@ -29,7 +35,7 @@ export default function ContractCaller() {
         return 'Try Again';
 
       default:
-        return 'Get Message';
+        return 'Create Property';
     }
   }, [fetchState]);
 
@@ -38,7 +44,7 @@ export default function ContractCaller() {
       <Alert.Heading>Contract Caller</Alert.Heading>
       <p>Let's see if we can interact with the contract.</p>
       {message && (
-        <p>
+        <p className="text-break">
           Message received: <b>{message}</b>
         </p>
       )}
--- lib/constants.ts
+++ lib/constants.ts
@@ -4,27 +4,300 @@ export const MAIN_URL = `${MAIN_ORIGIN}${MAIN_PATH}`;
 export const MAIN_TITLE = 'Decentralized Property Trading App';
 
 export const CONTRACT_ABI = [
-  { inputs: [], stateMutability: 'nonpayable', type: 'constructor' },
+  {
+    inputs: [
+      { internalType: 'string', name: 'name', type: 'string' },
+      { internalType: 'string', name: 'symbol', type: 'string' },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'owner',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'spender',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        internalType: 'uint256',
+        name: 'value',
+        type: 'uint256',
+      },
+    ],
+    name: 'Approval',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: 'uint256',
+        name: 'listingId',
+        type: 'uint256',
+      },
+      {
+        indexed: false,
+        internalType: 'string',
+        name: '_pinCode',
+        type: 'string',
+      },
+    ],
+    name: 'NewProperty',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'previousOwner',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'newOwner',
+        type: 'address',
+      },
+    ],
+    name: 'OwnershipTransferred',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'from',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'to',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        internalType: 'uint256',
+        name: 'value',
+        type: 'uint256',
+      },
+    ],
+    name: 'Transfer',
+    type: 'event',
+  },
+  {
+    inputs: [
+      { internalType: 'address', name: 'owner', type: 'address' },
+      { internalType: 'address', name: 'spender', type: 'address' },
+    ],
+    name: 'allowance',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'address', name: 'spender', type: 'address' },
+      { internalType: 'uint256', name: 'amount', type: 'uint256' },
+    ],
+    name: 'approve',
+    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'address', name: 'account', type: 'address' }],
+    name: 'balanceOf',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'propertyPrice',
+        type: 'uint256',
+      },
+      { internalType: 'uint256', name: 'listingId', type: 'uint256' },
+      { internalType: 'string', name: '_pinCode', type: 'string' },
+      { internalType: 'uint256', name: 'supply', type: 'uint256' },
+    ],
+    name: 'createProperty',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
   {
     inputs: [],
-    name: 'getMessage',
+    name: 'decimal',
+    outputs: [{ internalType: 'uint8', name: '', type: 'uint8' }],
+    stateMutability: 'pure',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'decimals',
+    outputs: [{ internalType: 'uint8', name: '', type: 'uint8' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'address', name: 'spender', type: 'address' },
+      {
+        internalType: 'uint256',
+        name: 'subtractedValue',
+        type: 'uint256',
+      },
+    ],
+    name: 'decreaseAllowance',
+    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'address', name: 'spender', type: 'address' },
+      { internalType: 'uint256', name: 'addedValue', type: 'uint256' },
+    ],
+    name: 'increaseAllowance',
+    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'name',
     outputs: [{ internalType: 'string', name: '', type: 'string' }],
     stateMutability: 'view',
     type: 'function',
   },
   {
     inputs: [],
-    name: 'message',
+    name: 'owner',
+    outputs: [{ internalType: 'address', name: '', type: 'address' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    name: 'propOwners',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    name: 'propSupplier',
+    outputs: [{ internalType: 'address', name: '', type: 'address' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'uint256', name: '', type: 'uint256' },
+      { internalType: 'address', name: '', type: 'address' },
+    ],
+    name: 'propToOwners',
+    outputs: [
+      { internalType: 'address', name: 'ownerAddr', type: 'address' },
+      { internalType: 'uint256', name: 'supply', type: 'uint256' },
+      { internalType: 'bool', name: 'isMaintaing', type: 'bool' },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    name: 'properties',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: 'propertyPrice',
+        type: 'uint256',
+      },
+      { internalType: 'string', name: 'pinCode', type: 'string' },
+      { internalType: 'uint256', name: 'listingId', type: 'uint256' },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'renounceOwnership',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'symbol',
     outputs: [{ internalType: 'string', name: '', type: 'string' }],
     stateMutability: 'view',
     type: 'function',
   },
   {
-    inputs: [{ internalType: 'string', name: 'newMessage', type: 'string' }],
-    name: 'setMessage',
+    inputs: [],
+    name: 'totalSupply',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'address', name: '_recipient', type: 'address' },
+      { internalType: 'uint256', name: 'supply', type: 'uint256' },
+      { internalType: 'uint256', name: 'listingId', type: 'uint256' },
+    ],
+    name: 'transfer',
+    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'address', name: 'to', type: 'address' },
+      { internalType: 'uint256', name: 'amount', type: 'uint256' },
+    ],
+    name: 'transfer',
+    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'address', name: 'from', type: 'address' },
+      { internalType: 'address', name: 'to', type: 'address' },
+      { internalType: 'uint256', name: 'amount', type: 'uint256' },
+    ],
+    name: 'transferFrom',
+    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'address', name: 'newOwner', type: 'address' }],
+    name: 'transferOwnership',
     outputs: [],
     stateMutability: 'nonpayable',
     type: 'function',
   },
 ];
-export const CONTRACT_ADDRESS = '0x764094175256Fb3fB375fF1202b924348b7c81F4';
+export const CONTRACT_ADDRESS = '0x7a8Fc944Cbaf4A8357356bA1bded0b9652b690d2';
+export const CONTRACT_CREATE_PROP_GAS = 15000000;
