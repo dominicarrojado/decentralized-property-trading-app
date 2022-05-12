2c21beb163711214a42b8ef81193cb5e5067b81e
Dominic Arrojado <dominicarrojado@gmail.com>
2022-05-12 23:04:56 +0800

    allow transfer property shares and minor refactor

---
+++ components/transferShares.tsx
@@ -0,0 +1,99 @@
+import React, { useContext, useMemo, useState } from 'react';
+import { Alert, Button, Form, FormGroup, FormLabel } from 'react-bootstrap';
+import { CONTRACT_CREATE_PROP_GAS } from '../lib/constants';
+import StoreContext from '../lib/context';
+import { getContract } from '../lib/eth';
+import { FetchState } from '../lib/types';
+
+export default function TransferShares() {
+  const context = useContext(StoreContext);
+  const [fetchState, setFetchState] = useState(FetchState.DEFAULT);
+  const [message, setMessage] = useState('');
+  const [listingId, setListingId] = useState('');
+  const [addressId, setAddressId] = useState('');
+  const [transferSupply, setTransferSupply] = useState(0);
+  const getMessage = async () => {
+    try {
+      setFetchState(FetchState.LOADING);
+
+      const contract = await getContract({
+        from: context.account?.addressId,
+        gas: CONTRACT_CREATE_PROP_GAS,
+      });
+      const res = await contract.methods
+        .transfer(addressId, transferSupply, listingId)
+        .send();
+
+      setListingId('');
+      setAddressId('');
+      setTransferSupply(0);
+      setMessage(JSON.stringify(res));
+      setFetchState(FetchState.SUCCESS);
+    } catch (err) {
+      console.error(err);
+      setFetchState(FetchState.ERROR);
+    }
+  };
+  const buttonText = useMemo(() => {
+    switch (fetchState) {
+      case FetchState.LOADING:
+        return 'Waiting...';
+
+      case FetchState.ERROR:
+        return 'Try Again';
+
+      default:
+        return 'Transfer Property Shares';
+    }
+  }, [fetchState]);
+
+  return (
+    <Alert variant="secondary">
+      <Alert.Heading>Transfer Property Shares</Alert.Heading>
+      <p>
+        Transfer your shares from your existing properties to another owner.
+      </p>
+      <Form>
+        <Form.Group className="mb-3" controlId="addressId">
+          <Form.Label>Listing ID:</Form.Label>
+          <Form.Control
+            type="text"
+            value={listingId}
+            onChange={(e) => setListingId(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="addressId">
+          <Form.Label>Transfer to:</Form.Label>
+          <Form.Control
+            type="text"
+            value={addressId}
+            onChange={(e) => setAddressId(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Transfer amount:</Form.Label>
+          <Form.Control
+            type="number"
+            value={transferSupply}
+            onChange={(e) => setTransferSupply(Number(e.target.value))}
+          />
+        </Form.Group>
+      </Form>
+      {message && (
+        <p className="text-break">
+          Message received: <b>{message}</b>
+        </p>
+      )}
+      <hr />
+      <div className="d-flex justify-content-end">
+        <Button
+          variant="secondary"
+          onClick={getMessage}
+          disabled={fetchState === FetchState.LOADING}
+        >
+          {buttonText}
+        </Button>
+      </div>
+    </Alert>
+  );
+}
--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,12 +1,14 @@
-import ContractCaller from '../components/contractCaller';
+import CreateProperty from '../components/createProperty';
 import MetaMaskConnected from '../components/metaMaskConnected';
+import TransferShares from '../components/transferShares';
 import Welcome from '../components/welcome';
 
 export default function Home() {
   return (
     <MetaMaskConnected>
       <Welcome />
-      <ContractCaller />
+      <CreateProperty />
+      <TransferShares />
     </MetaMaskConnected>
   );
 }
