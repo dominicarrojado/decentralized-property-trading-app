7af1d00d67080ecc0368cc639f8477cc1bf4490f
Dominic Arrojado <dominicarrojado@gmail.com>
2022-05-13 10:11:38 +0800

    hide release dividends button for demo

--- pages/properties/create.page.tsx
+++ pages/properties/create.page.tsx
@@ -1,9 +1,8 @@
 import { useRouter } from 'next/router';
-import React, { useContext, useEffect, useMemo, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { Button, Alert, Badge, Spinner } from 'react-bootstrap';
 import { getContract } from '../../lib/eth';
 import MetaMaskConnected from '../../components/metaMaskConnected';
-import StoreContext from '../../lib/context';
 import { CONTRACT_CREATE_PROP_GAS } from '../../lib/constants';
 import { FetchState } from '../../lib/types';
 import { useCurrentAddressId } from '../../lib/custom-hooks';
@@ -12,7 +11,6 @@ export default function CreateProperty() {
   const autoCompleteDoneRef = useRef(false);
   const router = useRouter();
   const { autoComplete } = router.query;
-  const context = useContext(StoreContext);
   const currentAddressId = useCurrentAddressId();
   const [fetchState, setFetchState] = useState(FetchState.DEFAULT);
   const [message, setMessage] = useState({
@@ -69,24 +67,6 @@ export default function CreateProperty() {
     }
   }, [fetchState]);
 
-  const dividentCheck = async () => {
-    const contract = await getContract({
-      from: context.account?.addressId,
-      gas: CONTRACT_CREATE_PROP_GAS,
-    });
-    const res = await contract.methods
-      .createProperty(10000000, 23451243, '550321', 10)
-      .send();
-
-    console.log('create property', res);
-
-    const divRes = await contract.methods
-      .releaseDividend()
-      .send({ value: 500000000, gas: 50000 });
-
-    console.log('divi', divRes);
-  };
-
   useEffect(() => {
     const autoCompleteDone = autoCompleteDoneRef.current;
 
@@ -125,11 +105,6 @@ export default function CreateProperty() {
           </Alert>
         </div>
       )}
-      <div>
-        <Button variant="secondary" onClick={dividentCheck}>
-          Release Divident
-        </Button>
-      </div>
     </MetaMaskConnected>
   );
 }
